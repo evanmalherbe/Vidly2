fbcccd0963bf404c035b713bf626a999d6b5cccc
Evan Malherbe <evan@ix.co.za>
2023-06-14 21:52:34 +0200

    Started creating api controller

---
+++ Controllers/Api/CustomersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Vidly2.Data;
+using Vidly2.Models;
+
+namespace Vidly2.Controllers.Api
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CustomersController : ControllerBase
+	{
+		private ApplicationDbContext _context;
+
+		public CustomersController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		// GET /api/customers
+		public IEnumerable<Customer> GetCustomers()
+		{
+			return _context.Customers.ToList();
+		}
+
+		// GET /api/customers/1
+		public Customer GetCustomer(int id)
+		{
+			var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+			if (customer == null)
+			{
+				throw new HttpRequestException(HttpStatusCode.NotFound.ToString());
+			}
+
+			return customer;
+		}
+
+		// POST /api/customers
+		//[HttpPost]
+		//public Customer CreateCustomer(Customer customer)
+		//{
+			
+		//}
+	}
+}
